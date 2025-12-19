7f7c50ac6590e5d8bb469c97741e25a0651c985d
scorpio <email@example.com>
2025-12-19 22:42:31 +0100

    .

--- ex3/ft_finally_block.py
+++ ex3/ft_finally_block.py
@@ -1,25 +1,37 @@
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    pass
+
+
+class WaterError(GardenError):
+    pass
+
+
 def water_plants(plant_list):
-    print("Opening watering system")
-    for plant in plant_list:
-        try:
-            print("Watering", plant)
-        except 
-    print("Closing watering system (cleanup)")
+    try:
+        print("Opening watering system")
+        for plant in plant_list:
+            if plant != None:
+                print("Watering", plant)
+            else:
+                raise WaterError("Error: Cannot water None - invalid plant!") 
+    except WaterError as error:
+        print(error)
+    finally:
+        print("Closing watering system (cleanup)")
 
+def test_watering_system():
+    ps =[
+            "tomato",
+            "lettuce",
+            "carrots",
+            ]
+    water_plants(ps)
+    print("===================================fucking test\n===================================================================")
+    ps[1] = None
+    water_plants(ps)
 
-
-
-
-
-
-
-
-
-
-
-ps =[
-        "tomato",
-        "lettuce",
-        "carrots",
-        ]
-water_plants(ps)
+test_watering_system()
